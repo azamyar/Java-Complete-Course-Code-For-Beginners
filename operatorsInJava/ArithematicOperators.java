cb57ffa16234e4761d125c6e2e4fce9924f0f299
Syed Azam Yar <42574974+azamyar@users.noreply.github.com>
2020-01-21 04:19:02 +0500

    Add files via upload

--- operatorsInJava/ArithematicOperators.java
+++ operatorsInJava/ArithematicOperators.java
@@ -3,15 +3,41 @@ package operatorsInJava;
 public class ArithematicOperators {
 	public static void main(String[]args) {
 	//Using Math formula
-	int a = 23;
-	int b = 45;
-	int c = a*a + b*b + 2 *a *b;
-	int d = (a+b) * (a+b);
+		//Bodmas Rule
+//	int a = 23;
+//	int b = 45;
+//	int c = a*a + b*b + 2 *a *b;
+//	int d = (a+b) * (a+b);
 	//int c = a + b ;
-	System.out.println(c);
-	System.out.println(d);
+//	System.out.println(c);
+//	System.out.println(d);
+//	int a = 23;
+//	int b = 45;
+//	double  c = (double)b/ (double) a; //DIVIDE
+//	System.out.println(c);
 	
+		//Modulus Operator
+		//Modulus Operator is basically give us remainder 
+//		int a = 23;
+//		int b = 45;
+//		int c = b % a;
+//		System.out.println("Remainder is " +c);
+        int a = 23;
+        int b = 45;
+        int c = ++b;
+        //int d = a--;
+        int d = --a;
+        System.out.println(c+ "   " +b);
+        System.out.println(d+ "   " +a);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
-	
-
 }
