479be046d1d8bef9da48b5fea1fa328c1c12325d
Syed Azam Yar <42574974+azamyar@users.noreply.github.com>
2020-01-23 03:00:49 +0500

    Bitwise Operators

---
+++ operatorsInJava/BitwiseOperators.java
@@ -0,0 +1,44 @@
+package operatorsInJava;
+
+public class BitwiseOperators {
+	public static void main(String[]args) {
+	//Bitiwise & operators
+		// 5 -> 101
+		// 6 -> 110
+		// 4 -> 100  -> & operaotors
+	
+//	int a = 5;
+//	int b = 6;
+//	int c = a & b ;
+//	System.out.println("This bitwise and operator is " +c);
+		// Or | operators
+//		int a =10;
+//		int b =13;
+//		int c =10 | 13;
+//		System.out.println("This is Bitwise OR | Operaotor "+c);
+		// Right Shift Operations
+		// left to right 1101 -> 0110 -> 0011 -> 0001 -> 0000 -> 0000
+//	    int a = 10;
+//	    int b = 13;
+//	    int c = b >> 2;   // -> right shift operations 
+	    //System.out.println(c);
+	    
+		//Left Shift Operations 
+		int a = 10;
+		int b = 13;
+		int c = b << 1;   // -> left shift operators means 1 is double.
+	    System.out.println(c);
+	    
+
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+	}
+}
