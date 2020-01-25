e469781b0fbdaeb44f2d8b4576a8b17f0f03b063
Syed Azam Yar <42574974+azamyar@users.noreply.github.com>
2020-01-26 02:15:46 +0500

    Students Pass Fail Program.
    Students Pass and Fail Programs using if else condition and taking input from user with the help of Scanner.

--- conditionalStatement/ConditionalOperators.java
+++ conditionalStatement/ConditionalOperators.java
@@ -1,5 +1,49 @@
 package conditionalStatement;
-
+//import java.util.Scanner;
+//
+//public class ConditionalOperators {
+//	public static void main(String[]args) {
+//		Scanner sc = new Scanner (System.in);
+//		
+//		/* Conditional Operators
+//          ==
+//          !=
+//          >
+//          <
+//          >=
+//          <=
+//          if (condition) {
+//          block of code to be executed if the condition is true.
+//          		}
+//	*/
+////		int noOfPetals = 6;
+////		if (noOfPetals % 2==0) {
+////			System.out.println("He loves me not");
+////		}
+////		else {
+////			System.out.println("He loves me");
+////			}
+////	       }
+////          }
+//		int noOfPetals = sc.nextInt();
+//             if (noOfPetals % 2==0) {
+//            	 System.out.println("He loves me");
+//            }
+//             else {
+//            	 System.out.println("He loves me not");
+//             }
+//	}
+//	}
+import java.util.Scanner;
 public class ConditionalOperators {
-
+	public static void main(String[]args) {
+		Scanner sc = new Scanner (System.in);
+		double marksStudent = sc.nextDouble();
+		if (marksStudent >60) { // If marks greater than 60 so student will be pass otherwise not
+			System.out.println("Student is Pass");
+		}
+		else {
+			System.out.println("Student is fail");
+		}
+	}
 }
